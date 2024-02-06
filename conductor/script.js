78112e7f87740ab7c5c33467a830d6bf3d5fe70e
Abusik019 <abusroyale@gmail.com>
2024-02-06 10:16:57 +0300

    pane fix

--- conductor/script.js
+++ conductor/script.js
@@ -1,31 +1,43 @@
+//aside movement
 const aside = document.querySelector('.aside');
+const main = document.querySelector('.main');
+const trigger = document.querySelector('.pane_trigger');
 let isResizing = false;
 let startX, initialWidth;
 
-document.addEventListener('mousemove', (event) => {
-    console.log(aside.offsetWidth)
-    console.log(event.pageX)
-    if (isResizing) {
-        const newWidth = Math.max(initialWidth + (event.pageX - startX), 250);
-        aside.style.width = newWidth + 'px';
-    } else if (event.pageX < aside.offsetWidth && event.pageX > aside.offsetWidth - 10) {
-        aside.style.cursor = 'w-resize';
-        aside.addEventListener('mousedown', startResize);
-    } else {
-        aside.style.cursor = 'default';
-        aside.removeEventListener('mousedown', startResize);
-    }
-});
 
-document.addEventListener('mouseup', () => {
-    isResizing = false;
-    aside.style.cursor = 'default';
-});
+trigger.addEventListener('mousedown', startResize);
+document.addEventListener('mouseup', stopResize);
+document.addEventListener('mousemove', resize);
 
-function startResize(event) {
+function startResize(event){
     isResizing = true;
     startX = event.pageX;
     initialWidth = aside.offsetWidth;
-    aside.style.cursor = 'w-resize';
+    event.preventDefault();
+
+    trigger.classList.add('active');
 }
 
+function resize(event){
+    if(isResizing){
+        const newWidth = Math.min(Math.max(initialWidth + (event.pageX - startX), 250), 500);
+        aside.style.width = newWidth + 'px'
+        main.style.width = `calc(100% - ${newWidth}px)`
+
+        trigger.style.left = `calc(${newWidth}px - 4px)`
+    }
+}
+
+function stopResize(){
+    isResizing = false;
+    trigger.classList.remove('active');
+}
+
+
+//create file or folder
+const createBtn = document.getElementById('create');
+
+createBtn.addEventListener('click', () => {
+
+});
